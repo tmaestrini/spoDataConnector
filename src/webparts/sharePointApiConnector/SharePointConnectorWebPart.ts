8ec2e455b430b05bd1741ff87cbde3e0ec8aa735
Tobias Maestrini <tobias@bee365.ch>
2024-11-25 02:13:47 +0100

    change SharePoint references as static copies from graphApiConnector webpart for scaffolding purpose

--- src/webparts/sharePointApiConnector/SharePointConnectorWebPart.ts
+++ src/webparts/sharePointApiConnector/SharePointConnectorWebPart.ts
@@ -1,74 +1,104 @@
 import * as React from 'react';
 import * as ReactDom from 'react-dom';
-import { Version } from '@microsoft/sp-core-library';
+import { DisplayMode, Version } from '@microsoft/sp-core-library';
 import {
+  DynamicDataSharedDepth,
   type IPropertyPaneConfiguration,
+  PropertyPaneDropdown,
+  PropertyPaneDynamicField,
+  PropertyPaneDynamicFieldSet,
+  PropertyPaneLabel,
   PropertyPaneTextField
 } from '@microsoft/sp-property-pane';
-import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
-import { IReadonlyTheme } from '@microsoft/sp-component-base';
+import { IDynamicDataCallables, IDynamicDataPropertyDefinition } from '@microsoft/sp-dynamic-data';
+import { BaseClientSideWebPart, IWebPartPropertiesMetadata } from '@microsoft/sp-webpart-base';
+import { DynamicProperty, IReadonlyTheme } from '@microsoft/sp-component-base';
+import { MSGraphClientV3 } from '@microsoft/sp-http';
 
 import * as strings from 'SharePointConnectorWebPartStrings';
 import SharePointConnector from './components/SharePointConnector';
 import { ISharePointConnectorProps } from './components/ISharePointConnectorProps';
+import { SharePointError, SharePointResult } from './models/types';
 
-export interface ISharePointConnectorWebPartWebPartProps {
-  description: string;
+export interface IGraphConnectorWebPartProps {
+  sourceSelector: 'none' | 'dynamicData';
+  dataSource?: DynamicProperty<undefined>;
+
+  api: string;
+  version: 'v1.0' | 'beta';
+  filter?: string;
+  select?: string;
+  expand?: string;
 }
 
-export default class SharePointConnectorWebPartWebPart extends BaseClientSideWebPart<ISharePointConnectorWebPartWebPartProps> {
+export default class GraphConnectorWebPart extends BaseClientSideWebPart<IGraphConnectorWebPartProps> implements IDynamicDataCallables {
 
-  private _isDarkTheme: boolean = false;
-  private _environmentMessage: string = '';
+  private sharePointClient: MSGraphClientV3;
+  private sharePointData: SharePointResult;
+  private dataSourceValues: undefined;
 
   public render(): void {
+    this.tryFetchDataSourceValues();
+
     const element: React.ReactElement<ISharePointConnectorProps> = React.createElement(
       SharePointConnector,
       {
-        description: this.properties.description,
-        isDarkTheme: this._isDarkTheme,
-        environmentMessage: this._environmentMessage,
-        hasTeamsContext: !!this.context.sdks.microsoftTeams,
-        userDisplayName: this.context.pageContext.user.displayName
+        dataFromDynamicSource: this.dataSourceValues,
+        api: this.properties.api,
+        version: this.properties.version,
+        filter: this.properties.filter,
+        select: this.properties.select,
+        expand: this.properties.expand,
+        sharePointClient: this.sharePointClient,
+
+        onSharePointDataResult: (data: SharePointResult | SharePointError) => {
+          if (data.type === 'result') {
+            delete (data as { type?: string }).type; // delete type property for better readability
+            this.sharePointData = data as SharePointResult;
+          } else {
+            console.error(data);
+          }
+
+          this.context.dynamicDataSourceManager.notifyPropertyChanged('sharePointData');
+        },
       }
     );
 
     ReactDom.render(element, this.domElement);
   }
 
-  protected onInit(): Promise<void> {
-    return this._getEnvironmentMessage().then(message => {
-      this._environmentMessage = message;
-    });
+  private tryFetchDataSourceValues(): void {
+    if (this.properties.sourceSelector === 'dynamicData') {
+      this.dataSourceValues = this.properties.dataSource?.tryGetValue();
+    } else {
+      this.dataSourceValues = undefined;
+    }
   }
 
+  protected async onInit(): Promise<void> {
+    this.sharePointClient = await this.context.msGraphClientFactory.getClient('3');
+    this.context.dynamicDataSourceManager.initializeSource(this);
+    return Promise.resolve();
+  }
 
+  public getPropertyDefinitions(): ReadonlyArray<IDynamicDataPropertyDefinition> {
+    return [
+      { id: 'sharePointData', title: 'Graph Data result' },
+    ]
+  }
 
-  private _getEnvironmentMessage(): Promise<string> {
-    if (!!this.context.sdks.microsoftTeams) { // running in Teams, office.com or Outlook
-      return this.context.sdks.microsoftTeams.teamsJs.app.getContext()
-        .then(context => {
-          let environmentMessage: string = '';
-          switch (context.app.host.name) {
-            case 'Office': // running in Office
-              environmentMessage = this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentOffice : strings.AppOfficeEnvironment;
-              break;
-            case 'Outlook': // running in Outlook
-              environmentMessage = this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentOutlook : strings.AppOutlookEnvironment;
-              break;
-            case 'Teams': // running in Teams
-            case 'TeamsModern':
-              environmentMessage = this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentTeams : strings.AppTeamsTabEnvironment;
-              break;
-            default:
-              environmentMessage = strings.UnknownEnvironment;
-          }
+  public getPropertyValue(propertyId: string): SharePointResult {
+    if (propertyId === 'sharePointData') return this.sharePointData;
+    throw new Error(`property '${propertyId}' not found`);
+  }
 
-          return environmentMessage;
-        });
-    }
-
-    return Promise.resolve(this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentSharePoint : strings.AppSharePointEnvironment);
+  // dynamic data method
+  protected get propertiesMetadata(): IWebPartPropertiesMetadata {
+    return {
+      'dataSource': {
+        dynamicPropertyType: 'object'
+      },
+    };
   }
 
   protected onThemeChanged(currentTheme: IReadonlyTheme | undefined): void {
@@ -76,7 +106,7 @@ export default class SharePointConnectorWebPartWebPart extends BaseClientSideWeb
       return;
     }
 
-    this._isDarkTheme = !!currentTheme.isInverted;
+    this.domElement.style.setProperty('--displayMode', this.displayMode === DisplayMode.Read ? 'none' : 'inherit');
     const {
       semanticColors
     } = currentTheme;
@@ -97,6 +127,12 @@ export default class SharePointConnectorWebPartWebPart extends BaseClientSideWeb
     return Version.parse('1.0');
   }
 
+  protected onPropertyPaneFieldChanged(propertyPath: string, oldValue: string, newValue: string): void {
+    if (propertyPath === 'sourceSelector') {
+      this.tryFetchDataSourceValues()
+    }
+  }
+
   protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
     return {
       pages: [
@@ -106,16 +142,76 @@ export default class SharePointConnectorWebPartWebPart extends BaseClientSideWeb
           },
           groups: [
             {
-              groupName: strings.BasicGroupName,
+              groupName: strings.DataSource.GroupNameLabel,
               groupFields: [
-                PropertyPaneTextField('description', {
-                  label: strings.DescriptionFieldLabel
-                })
-              ]
-            }
-          ]
-        }
-      ]
+                PropertyPaneLabel('dataSourceDescriptionLabel', {
+                  text: strings.DataSource.DataSourceDescriptionText,
+                }),
+                PropertyPaneDropdown('sourceSelector', {
+                  label: strings.DataSource.SourceSelectorLabel,
+                  options: [
+                    { key: 'none', text: 'Static content (None)' },
+                    { key: 'dynamicData', text: 'Dynamic data (Internal data source)' },
+                  ],
+                }),
+                ...(this.properties.sourceSelector === 'dynamicData' ? [PropertyPaneDynamicFieldSet({
+                  label: "",
+                  fields: [
+                    PropertyPaneDynamicField('dataSource', {
+                      label: strings.DataSource.DynamicDataLabel,
+                      propertyValueDepth: DynamicDataSharedDepth.Property,
+                      sourcesLabel: "Available data sources",
+                    }),
+                  ],
+                })] : []),
+              ],
+            },
+            {
+              groupName: strings.SharePointApi.BasicGroupName,
+              groupFields: [
+                ...(this.properties.sourceSelector === 'dynamicData' ? [PropertyPaneLabel('dataSourceSelectedLabel', {
+                  text: strings.SharePointApi.MainDescriptionText,
+                })] : []),
+                ...(this.properties.sourceSelector === 'dynamicData' ? [PropertyPaneLabel('dataSourceSelectedLabel', {
+                  text: `Example: {{siteTitle}} or {{value}}.`,
+                })] : []),
+                PropertyPaneDropdown('version', {
+                  label: strings.SharePointApi.VersionLabel,
+                  options: [
+                    { key: 'v1.0', text: 'v1.0' },
+                    { key: 'beta', text: 'beta' },
+                  ],
+                }),
+                PropertyPaneTextField('api', {
+                  label: `${strings.SharePointApi.ApiLabel} ${this.dataSourceValues ? '👇 use dynamic data' : ''}`,
+                  placeholder: '/me, /me/manager, /me/joinedTeams, /users',
+                  description: `https://graph.microsoft.com${this.properties.api}`,
+                  multiline: true,
+
+                }),
+                PropertyPaneTextField('filter', {
+                  label: `${strings.SharePointApi.FilterLabel} ${this.dataSourceValues ? '👇 use dynamic data' : ''}`,
+                  placeholder: `emailAddress eq 'john@contoso.com'`,
+                  multiline: true,
+                }),
+                PropertyPaneTextField('select', {
+                  label: strings.SharePointApi.SelectLabel,
+                  placeholder: 'givenName,surname'
+                }),
+                PropertyPaneTextField('expand', {
+                  label: strings.SharePointApi.ExpandLabel,
+                  placeholder: 'members',
+                }),
+              ],
+            },
+          ],
+        },
+      ],
     };
   }
+
+  protected get disableReactivePropertyChanges(): boolean {
+    return true;
+  }
+
 }
--- src/webparts/sharePointApiConnector/loc/en-us.js
+++ src/webparts/sharePointApiConnector/loc/en-us.js
@@ -1,16 +1,35 @@
-define([], function() {
+define([], function () {
   return {
-    "PropertyPaneDescription": "Description",
-    "BasicGroupName": "Group Name",
-    "DescriptionFieldLabel": "Description Field",
-    "AppLocalEnvironmentSharePoint": "The app is running on your local environment as SharePoint web part",
-    "AppLocalEnvironmentTeams": "The app is running on your local environment as Microsoft Teams app",
-    "AppLocalEnvironmentOffice": "The app is running on your local environment in office.com",
-    "AppLocalEnvironmentOutlook": "The app is running on your local environment in Outlook",
-    "AppSharePointEnvironment": "The app is running on SharePoint page",
-    "AppTeamsTabEnvironment": "The app is running in Microsoft Teams",
-    "AppOfficeEnvironment": "The app is running in office.com",
-    "AppOutlookEnvironment": "The app is running in Outlook",
-    "UnknownEnvironment": "The app is running in an unknown environment"
+    PropertyPaneDescription: "Display content from SharePoint API and make it available for other webparts.",
+    DataSource: {
+      GroupNameLabel: "Data Source",
+      DataSourceDescriptionText: `You can optionally connect to data sources (page environment or other webparts on this page that provide 
+      data source functionality) by selecting 'Dynamic data (Internal data source)' in the dropdown field 'Type of content'.`,
+      SourceSelectorLabel: "Type of content",
+      DynamicDataLabel: "Dynamic data (Internal data source)",
+    },
+    SharePointApi: {
+      BasicGroupName: "SharePoint API request",
+      MainDescriptionText: `Ingest the desired attribute from the result of the selected data source in the API field.
+                  Use the curly brackets {{...}} as a placeholder to insert the value.`,
+      ApiLabel: "API endpoint",
+      FilterLabel: "Filter ($filter attribute)",
+      SelectLabel: "Select ($select attribute)",
+      VersionLabel: "SharePoint version",
+      ExpandLabel: "Expand ($expand attribute)",
+    },
+    SharePointConnector: {
+      ShowSharePointResultsLabel: "Show result(s) from SharePoint",
+      ShowDynamicDataLabel: "Show dynamic data"
+    },
+    AppLocalEnvironmentSharePoint: "The app is running on your local environment as SharePoint web part",
+    AppLocalEnvironmentTeams: "The app is running on your local environment as Microsoft Teams app",
+    AppLocalEnvironmentOffice: "The app is running on your local environment in office.com",
+    AppLocalEnvironmentOutlook: "The app is running on your local environment in Outlook",
+    AppSharePointEnvironment: "The app is running on SharePoint page",
+    AppTeamsTabEnvironment: "The app is running in Microsoft Teams",
+    AppOfficeEnvironment: "The app is running in office.com",
+    AppOutlookEnvironment: "The app is running in Outlook",
+    UnknownEnvironment: "The app is running in an unknown environment"
   }
 });
