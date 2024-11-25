914242d7573cd686f4b348edb40370f569c27f43
Tobias Maestrini <tobias@bee365.ch>
2024-11-25 01:40:59 +0100

    initial commit

---
+++ src/webparts/sharePointApiConnector/components/ISharePointConnectorProps.ts
@@ -0,0 +1,7 @@
+export interface ISharePointConnectorProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
